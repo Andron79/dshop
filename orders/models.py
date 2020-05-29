1d6013a5d1126f371220ed30a59c6195c4737c21
Andron79 <av7779av@gmail.com>
2020-05-29 10:25:47 +0300

    Исправление Оформления заказа

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-#from accounts.models import UserProfile
 from dshop.models import Product
 
 
---
+++ orders/tests/test_views.py
@@ -0,0 +1,3 @@
+from orders.models import Order, OrderItem
+from django.test import TestCase
+
