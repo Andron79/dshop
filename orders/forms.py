76b897942df13ddc1483dedb37d327167b4063ca
Andron79 <av7779av@gmail.com>
2020-06-14 18:13:18 +0300

    FINAL!!!!

--- orders/forms.py
+++ orders/forms.py
@@ -1,6 +1,6 @@
-from django.forms import TextInput
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django import forms
-
 from .models import Order
 
 
@@ -8,3 +8,9 @@ class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'phone', 'city']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Save'))
