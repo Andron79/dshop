0292dac0aea9ea245a32c93fa2bd16a0f1104685
Andron79 <av7779av@gmail.com>
2020-08-17 18:26:51 +0300

    ver 2.0

--- dshop/urls.py
+++ dshop/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from dshop import views
+from dshop.views import ProductDetail #, CategoryView
 
 app_name = 'dshop'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('product_list/', CategoryView.as_view(), name='product_list'),
     path('product_list/', views.product_list, name='product_list'),
-    path('product/<slug>/', views.product_detail, name='product_detail'),
+    path('product/<slug:slug>/', ProductDetail.as_view(), name='product_detail'),
+    # path('<slug:slug>/', CategoryView.as_view(), name='product_list_by_category'),
     path('<slug>/', views.product_list, name='product_list_by_category'),
 
 ]
--- dshop/views.py
+++ dshop/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category, Article
 from cart.forms import CartAddProductForm
+from django.views.generic import ListView, DetailView, FormView
 
 
-def index(request):
-    template = 'dshop/index.html'
-    articles = Article.objects.all().order_by('published_at')
-    return render(request, template,
-                  context={'articles': articles})
+class IndexView(ListView):
+    template_name = 'dshop/index.html'
+    queryset = Article.objects.all().order_by('published_at')
+    context_object_name = 'articles'
+
+
+# class CategoryView(ListView):
+#
+#     model = Category
+#     slug_field = 'pk'
+#     products = Product.objects.filter(available=True)
+#     category = get_object_or_404(Category, slug=slug_field)
+#     template_name = 'dshop/product/list.html'
+#
+#     def get_queryset(self):
+#         query_set = self.model.objects.filter(category=self.kwargs.get('category'))
+#         return query_set
+#     # queryset = Product.objects.filter(available=True).filter(category__in=category.get_descendants(include_self=True))
+#     context_object_name = 'products'
+#     # def get_object(self, queryset=queryset):
+#     #     slug = self.kwargs.get(self.slug_url_kwarg, None)
+#     #     return queryset.get(slug=slug)
+#     # category = get_object_or_404(Category, slug=slug)
+#     # queryset = Product.objects.filter(available=True) #.filter(category__in=category.get_descendants(include_self=True))
 
 
 def product_list(request, slug='root'):
@@ -20,28 +40,21 @@ def product_list(request, slug='root'):
                    'products': products})
 
 
-def product_detail(request, slug):
-    template = 'dshop/product/detail.html'
-    product = get_object_or_404(Product,
-                                slug=slug,
-                                available=True)
-    cart_product_form = CartAddProductForm()
-    return render(request, template,
-                  {'product': product,
-                   'cart_product_form': cart_product_form})
+class ProductDetail(FormView, DetailView):
+    model = Product
+    template_name = 'dshop/product/detail.html'
+    form_class = CartAddProductForm
+    queryset = Product.objects.filter(available=True)
+    context_object_name = 'product'
 
 
-def articles_list(request):
-    template = 'dshop/articles_list.html'
-    articles = Article.objects.all()
-    return render(request, template,
-                  context={
-                      'articles': articles})
+class ArticleView(ListView):
+    template_name = 'dshop/articles_list.html'
+    queryset = Article.objects.all().order_by('published_at')
+    context_object_name = 'articles'
 
 
-def articles_detail(request, slug):
-    template = 'dshop/article_detail.html'
-    article = get_object_or_404(Article,
-                                slug=slug)
-    return render(request, template,
-                  {'article': article})
+class ArticleDetail(DetailView):
+    model = Article
+    template_name = 'dshop/article_detail.html'
+    context_object_name = 'article'
